a69d045e55297636bc9e966ee551caef9947b230
Robert Schwenn <devel@rstyx.de>
2013-11-24 14:28:44 +0100

    docs: log4net api removed

--- LoggingConsole/doc/SHFB/Help/Install_V2.bat
+++ LoggingConsole/doc/SHFB/Help/Install_V2.bat
@@ -4,7 +4,7 @@ CLS
 REM Help Viewer 2.0
 REM Uninstall first in case it is already there.  If not, it won't install below.  We'll ignore any error output
 REM by redirecting it to NUL.
-start "Uninstall Help Viewr Content" "C:\Program Files (x86)\Microsoft Help Viewer\v2.1\HlpCtntMgr.exe" /catalogName VisualStudio12 /locale de-de /wait 0 /operation uninstall /vendor "Rstyx" /productName "Frameworks and Applications" /bookList "LoggingConsole" > NUL
+"C:\Program Files (x86)\Microsoft Help Viewer\v2.1\HlpCtntMgr.exe" /catalogName VisualStudio12 /locale en-us /wait 0 /operation uninstall /vendor "Rstyx" /productName "Frameworks and Applications" /bookList "LoggingConsole" > NUL
 
 REM Install the new content.
-start "Install Help Viewr Content" "C:\Program Files (x86)\Microsoft Help Viewer\v2.1\HlpCtntMgr.exe" /catalogName VisualStudio12 /locale de-de /wait 0 /operation install /sourceUri "%CD%\LoggingConsole.msha"
+start "Install Help Viewr Content" "C:\Program Files (x86)\Microsoft Help Viewer\v2.1\HlpCtntMgr.exe" /catalogName VisualStudio12 /locale en-us /wait 0 /operation install /sourceUri "%CD%\LoggingConsole.msha"
