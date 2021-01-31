77f42a7345520264205765a2e2b7198dba15fb3f
Robert Schwenn <devel@rstyx.de>
2021-01-31 21:54:40 +0100

    Expander: Changed cursor to "Hand" and added tool tip

--- LoggingConsole/My Project/Settings.Designer.vb
+++ LoggingConsole/My Project/Settings.Designer.vb
@@ -236,7 +236,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property ShowConsoleOnError() As Boolean
             Get
                 Return CType(Me("ShowConsoleOnError"),Boolean)
