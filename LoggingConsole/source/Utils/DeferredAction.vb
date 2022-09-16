70a8afea57a601fa780b2f60419e1560ced0af15
Robert Schwenn <devel@rstyx.de>
2022-09-16 20:43:14 +0200

    DeferredAction updated to the version of Rstyx.Utilities

--- LoggingConsole/source/Utils/DeferredAction.vb
+++ LoggingConsole/source/Utils/DeferredAction.vb
@@ -34,7 +34,7 @@ Public Class DeferredAction
         
         ''' <summary> Creates a new DeferredAction running in current thread. </summary>
          ''' <param name="action"> The action that is intended to be invoked deferred. </param>
-         ''' <exception cref="T:System.ArgumentNullException"> <paramref name="action"/> is <see langword="null"/>. </exception>
+         ''' <exception cref="System.ArgumentNullException"> <paramref name="action"/> is <see langword="null"/>. </exception>
         Public Sub New(action As Action)
             Me.New(action, System.Windows.Threading.Dispatcher.CurrentDispatcher)
         End Sub
@@ -42,14 +42,14 @@ Public Class DeferredAction
         ''' <summary> Creates a new DeferredAction running in a given thread. </summary>
          ''' <param name="action">     The action that is intended to be invoked deferred. </param>
          ''' <param name="dispatcher"> The dispatcher that will invoke the action (when time has come). </param>
-         ''' <exception cref="T:System.ArgumentNullException"> <paramref name="action"/> is <see langword="null"/>. </exception>
-         ''' <exception cref="T:System.ArgumentNullException"> <paramref name="dispatcher"/> is <see langword="null"/>. </exception>
+         ''' <exception cref="System.ArgumentNullException"> <paramref name="action"/> is <see langword="null"/>. </exception>
+         ''' <exception cref="System.ArgumentNullException"> <paramref name="dispatcher"/> is <see langword="null"/>. </exception>
         Public Sub New(action As Action, dispatcher As System.Windows.Threading.Dispatcher)
             
             If (action Is Nothing) Then Throw New ArgumentNullException("action")
             If (dispatcher Is Nothing) Then Throw New ArgumentNullException("dispatcher")
             
-            Me.timer = New System.Threading.Timer(New System.Threading.TimerCallback(Function() dispatcher.Invoke(action) ))
+            Me.timer = New System.Threading.Timer(New System.Threading.TimerCallback(Sub() dispatcher.Invoke(action) ))
         End Sub
         
     #End Region
@@ -61,13 +61,29 @@ Public Class DeferredAction
         ''' Repeated calls will reschedule the action, if it has not already been performed.
         ''' </summary>
          ''' <param name="delay"> The amount of time to wait before performing the action. </param>
-         ''' <exception cref="T:System.ObjectDisposedException">     This <see cref="T:DeferredAction"/> has been already disposed of. </exception>
-         ''' <exception cref="T:System.ArgumentOutOfRangeException"> <paramref name="delay"/> is less than -1. </exception>
-         ''' <exception cref="T:System.NotSupportedException">       <paramref name="delay"/> is greater than 4294967294. </exception>
+         ''' <exception cref="System.ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's timer) has been already disposed of. </exception>
+         ''' <exception cref="System.ArgumentOutOfRangeException"> <paramref name="delay"/> is less than -1. </exception>
+         ''' <exception cref="System.NotSupportedException">       <paramref name="delay"/> is greater than 4294967294 milliseconds. </exception>
         Public Sub Defer(delay As TimeSpan)
-            'If (Me.timer IsNot Nothing) Then
-                Me.timer.Change(delay, TimeSpan.FromMilliseconds(-1))
-            'End If
+            Me.timer.Change(delay, TimeSpan.FromMilliseconds(-1))
+        End Sub
+        
+        ''' <summary>
+        ''' Schedules the action for performing once after the specified delay.
+        ''' Repeated calls will reschedule the action, if it has not already been performed.
+        ''' </summary>
+         ''' <param name="delay"> The amount of time (in milliseconds) to wait before performing the action. </param>
+         ''' <exception cref="System.ObjectDisposedException">     This <see cref="DeferredAction"/> (resp. it's timer) has been already disposed of. </exception>
+         ''' <exception cref="System.ArgumentOutOfRangeException"> <paramref name="delay"/> is less than -1. </exception>
+         ''' <exception cref="System.NotSupportedException">       <paramref name="delay"/> is greater than 4294967294 milliseconds. </exception>
+        Public Sub Defer(delay As Double)
+            Me.timer.Change(TimeSpan.FromMilliseconds(delay), TimeSpan.FromMilliseconds(-1))
+        End Sub
+        
+        ''' <summary> Aborts an active schedule, if there is one. </summary>
+         ''' <exception cref="System.ObjectDisposedException"> This <see cref="DeferredAction"/> (resp. it's timer) has been already disposed of. </exception>
+        Public Sub Abort()
+            Me.timer.Change(TimeSpan.FromMilliseconds(-1), TimeSpan.FromMilliseconds(-1))
         End Sub
         
     #End Region
